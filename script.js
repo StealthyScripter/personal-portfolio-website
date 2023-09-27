7ad814492dcc27ea686b4b8e9370658956396333
danielmendes24 <145878071+danielmendes24@users.noreply.github.com>
2023-09-27 07:10:03 -0300

    Fixing script

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function clickMenu() {
     if (Menu.style.display == 'block') {
         Menu.style.display = 'none'
-        document.getElementById('button').innerHTML = "close"
+        document.getElementById('button').innerHTML = "menu"
     } else {
         Menu.style.display = 'block'
-        document.getElementById('button').innerHTML = "menu"
+        document.getElementById('button').innerHTML = "close"
         
     }}
